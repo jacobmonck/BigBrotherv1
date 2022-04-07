423743dfc09f8ba3c043789159f01f4f53442573
JacobMonck <jakeamonck@gmail.com>
2022-04-06 21:56:43 -0400

    feat: add authorization

--- src/api/__main__.py
+++ src/api/__main__.py
@@ -1,14 +1,19 @@
 from os import environ
 
+from hmac import compare_digest
 from aioredis import from_url
 from dotenv import load_dotenv
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, Request
+
+load_dotenv()
 
 from .impl.database import database
 from .routes import guilds_router
 
-load_dotenv()
+API_TOKEN = environ["API_TOKEN"]
 
+if not API_TOKEN:
+    raise Exception("API key not set")
 
 app = FastAPI(
     title="BigBrother Internal API",
@@ -38,3 +43,18 @@ async def shutdown() -> None:
 
     if database_.is_connected:
         await database_.disconnect()
+
+
+@app.middleware("http")
+async def check_authentication(request: Request, call_next) -> Response:
+    path = request.url.path
+
+    if path.startswith(("/openapi.json", "/docs")):
+        return await call_next(request)
+
+    auth = request.headers.get("Authorization")
+
+    if not (auth and compare_digest(auth, API_TOKEN)):
+        return Response(status_code=401)
+
+    return await call_next(request)
