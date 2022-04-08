66f2e488c597458536193265694310620f17fd95
JacobMonck <jakeamonck@gmail.com>
2022-04-08 16:26:17 -0400

    style: format code

--- src/utils/api_client.py
+++ src/utils/api_client.py
@@ -1,8 +1,8 @@
 from os import environ
 from typing import Optional
-from orjson import dumps
 
 from aiohttp import ClientSession
+from orjson import dumps
 
 
 class APIClient:
@@ -13,21 +13,26 @@ class APIClient:
     @property
     def _session(self) -> ClientSession:
         if self.__session is None or self.__session.closed:
-            self.__session = ClientSession(headers={"Authorization": environ["API_TOKEN"], }, raise_for_status=True)
+            self.__session = ClientSession(
+                headers={
+                    "Authorization": environ["API_TOKEN"],
+                },
+                raise_for_status=True,
+            )
 
         return self.__session
 
     async def _request(self, method: str, route: str, **kwargs) -> dict:
         async with self._session.request(method, self._api_url + route, **kwargs) as response:
             data = await response.json()
-        
+
             return data
 
     async def get_guild(self, guild_id: int) -> dict:
         return await self._request("GET", f"/guilds/{guild_id}")
-    
+
     async def update_guild(self, guild_id: int, data: dict) -> dict:
         return await self._request("POST", f"/guilds/{guild_id}", json=data)
-    
+
     async def delete_guild(self, guild_id: int) -> dict:
         return await self._request("DELETE", f"/guilds/{guild_id}")
